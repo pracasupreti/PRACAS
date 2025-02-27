e618e92e6ecce75a03f6c4fc69235363e10b810f
Akash1010 <akashdahal6969@gmail.com>
2025-02-27 20:58:17 +0545

    completed

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 
 const Sample = () => {
   return (
-    <div className='p-5 absolute top-10 ml-[210px] mr-[26px]  '>
-        <div className='flex mt-4 '>
+    <div className='p-5 absolute four:ml-[3px] 2xl:ml-[210px]  top-10 ml-[210px] mr-[26px]  '>
+        <div className='flex  four:mt-2 ten:mt-4 '>
 
 
        <img src="https://i0.wp.com/ridethewave.co/wp-content/uploads/2023/11/Inspiring-Headshot-Examples-for-Professionals-and-Graduates.png?fit=739%2C924&ssl=1"
             alt="headshot" 
             className='w-[110px] h-[100px] rounded-full'/>
 
-              <h1 className='font-bold text-4xl ml-16 mt-auto mb-auto'>Lou Alvarez</h1>
+              <h1 className='font-bold seven:text-4xl four:ml-6 four:text-2xl seven:ml-16 mt-auto mb-auto'>Lou Alvarez</h1>
        
        </div>
        <div className='flex mt-[90px] '>
-       <div className='  bg-orange-300 w-[257px] pl-5 pt-2 h-[120px] gap-0 '> 
-        <h1 className='text-2xl   '>Contact</h1>
+       <div className='  bg-orange-300 four:w-[100px] four:pl-2 four:pt-2 four:h-[89px] seven:w-[257px] pl-5 pt-2 seven:h-[120px] gap-0 '> 
+        <h1 className='seven:text-2xl four:text-sm  '>Contact</h1>
         <p className='mt-[-10px]'>Santa Clara, CA 95050</p>
               
         <p className='mt-[-19px]'><strong>Mobile:</strong> 555-555-555</p>
    
                 <p className=' mt-[-18px]' >example@example.com</p>
        </div>
+       <div className="border-l-2 border-gray-300 h-[1100px] min-h-screen absolute left-[305px] top-[260px]"></div>
 
        <div className='mt-[-9px] w-[70%] p-[20px] ml-8'>
        <h3>PROFESSIONAL SUMMARY</h3>
@@ -83,10 +84,57 @@ const Sample = () => {
     <p>August 2018- March 2020 <br/> 
       <span className='font-bold'>Special Education Volunteer, Santa Clara Children</span>
     </p>
-        
+    <p className='mt-[-14px]'>
+      <span className='font-bold'>Community Center</span> 
+      ,Santa Clara,CA
+      
+      </p>
+        <ul className='list-disc list-inside'>
+        <li>Facilitated administration and scoring of standardized tests and
+                        assessments to draw meaningful data regarding learning and growth.</li>
+                        <li> Kept open and frequent communication with 10 families to provide
+                        regular updates on child's academic progress and individualized
+                        needs.</li>
+                        <li>  Delivered efficient instruction by assisting with lesson planning and
+                        materials preparation.</li>
+
+        </ul>
       </div>
 
      </div>
+
+     <div className='flex mt-7'>
+      <div>
+      
+        <h3>languages</h3>
+        <p><strong>English:</strong>Native language</p>
+      <p>  <span className='font-bold '>Spanish :</span> <span className='ml-[96px]'>C1</span></p>
+      
+      <div className="flex justify-between px-2">
+      <div className="bg-orange-500 h-1 w-10"></div>
+      <div className="bg-orange-500 h-1 w-10"></div>
+      <div className="bg-orange-500 h-1 w-10"></div>
+      <div className="bg-orange-500 h-1 w-10"></div>
+      <div className="bg-orange-500 h-1 w-10"></div>
+      <div className="bg-blue-400 h-1 w-10"></div> 
+    </div>
+    <p>Advanced</p>
+      </div>
+
+      <div className='ml-[108px] mt-[72px]'>
+      <p>June 2017-July 2018 <br/>
+      <strong>Teaching Intern, Building Kids</strong>, San Bernandino, CA</p>
+      <ul className='list-disc list-inside'>
+                    <li>Served as mentor to students, answered questions, provided tutoring
+                        services and offered one-on-one lesson reviews.</li>
+                        <li>  Supported classroom teacher for over 25 students by moving desks,
+                        straightening books and handing out lesson supplies.</li>
+                        <li>  Handled clerical duties such as typing, filing, copying and preparing
+                        instructional material for students.</li>
+                   </ul>
+      </div>
+
+      </div>
         
        
     </div>
