1afd8c218dbc20fec7534be41579e5e7278cfb2b
Abhay Kumar Gupta <akgupta128125@gmail.com>
2025-03-12 14:09:27 +0545

    cv

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Sample1 from '../pages/cv/Sample1'
 import Sample2 from '../pages/cv/Sample2'
 import Sample3 from '../pages/cv/Sample3'
+import Sample4 from '../pages/cv/Sample4'
+import Sample5 from '../pages/cv/Sample5'
 
 function cv() {
   return (
@@ -12,6 +14,10 @@ function cv() {
       <Sample2 />
       <div className='h-20' />
       <Sample3 />
+      <div className='h-20' />
+      <Sample4 />
+      <div className='h-20' />
+      <Sample5 />
     </>
   )
 }
---
+++ src/pages/cv/Sample5.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sample5() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Sample5
