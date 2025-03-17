b934c3710410f1f0856a673489d4f4d5115a4ff5
Akash1010 <akashdahal6969@gmail.com>
2025-03-17 21:25:52 +0545

    prakash cv

--- src/components/Samples.jsx
+++ src/components/Samples.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sample_one from '../pages/samples/Sample_one';
 import { Sample_two } from '../pages/samples/sample_two';
+import Sample_three from '../pages/samples/sample_three';
 
 const Samples = ()=>{
 
@@ -10,6 +11,9 @@ const Samples = ()=>{
     <Sample_one />
     <div className='h-24' />
     <Sample_two />
+    <div className='h-24' />
+    <Sample_three />
+  
     </>
  )
 }
