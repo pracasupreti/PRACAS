ce8ae068b1950857a9df4f7564725a9a5305c860
Akash1010 <akashdahal6969@gmail.com>
2025-03-18 19:31:44 +0545

    4th sample

--- src/components/Samples.jsx
+++ src/components/Samples.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Sample_one from '../pages/samples/Sample_one';
 import { Sample_two } from '../pages/samples/sample_two';
 import Sample_three from '../pages/samples/sample_three';
+import Sample_Four from '../pages/samples/Sample_Four';
 
 const Samples = ()=>{
 
@@ -13,7 +14,8 @@ const Samples = ()=>{
     <Sample_two />
     <div className='h-24' />
     <Sample_three />
-  
+    <div className='h-24' />
+    <Sample_Four />
     </>
  )
 }
